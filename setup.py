69342a06b757fc091d96052b7f7ead03d5ac9255
shalomcrown <shalomcrown@gmail.com>
2014-11-22 21:38:30 +0200

    Build Debian source package.

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ from distutils.core import setup
 setup(name='homesec_client',
       version='0.1',
       py_modules=['homesec_client'],
+      author = 'Shalom Crown',
+      author_email='shalomcrown@gmail.com'
+      
       )
+
